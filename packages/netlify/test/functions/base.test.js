2ebb9b6a97190c7bda457453a5b26ec7a13c1fe7
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-10-30 15:55:23 +0100

    test(netlify): add base fixture and prepare test

---
+++ packages/netlify/test/functions/base.test.js
@@ -0,0 +1,19 @@
+// import { expect } from 'chai';
+// import fs from 'fs/promises';
+import { cli } from './test-utils.js';
+import { fileURLToPath } from 'url';
+
+const root = new URL('./fixtures/base/', import.meta.url).toString();
+
+describe('Base', () => {
+	before(async () => {
+		await cli('build', '--root', fileURLToPath(root));
+	});
+
+	it('Path is prepended by base', async () => {
+		// TODO: uncomment after fix is implemented
+		// const redir = await fs.readFile(new URL('./dist/_redirects', root), 'utf-8');
+		// const expr = new RegExp('/test/     /.netlify/functions/entry    200');
+		// expect(redir).to.match(expr);
+	});
+});
