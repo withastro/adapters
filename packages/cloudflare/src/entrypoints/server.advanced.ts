15e0455ae13ca3bb44b239caffb564aa40bba839
lilnasy <69170106+lilnasy@users.noreply.github.com>
2023-11-15 22:28:28 +0000

    re-indent

--- packages/cloudflare/src/entrypoints/server.advanced.ts
+++ packages/cloudflare/src/entrypoints/server.advanced.ts
@@ -39,35 +39,33 @@ export function createExports(manifest: SSRManifest) {
 			return env.ASSETS.fetch(request);
 		}
 
-		// REF: If routeData is undefined, the render method invokes the #renderError method
-		let routeData = app.match(request, { matchNotFound: true }) || undefined;
+		let routeData = app.match(request, { matchNotFound: true });
+			Reflect.set(
+				request,
+				Symbol.for('astro.clientAddress'),
+				request.headers.get('cf-connecting-ip')
+			);
 
-		Reflect.set(
-			request,
-			Symbol.for('astro.clientAddress'),
-			request.headers.get('cf-connecting-ip')
-		);
-
-		const locals: AdvancedRuntime = {
-			runtime: {
-				waitUntil: (promise: Promise<any>) => {
-					context.waitUntil(promise);
+			const locals: AdvancedRuntime = {
+				runtime: {
+					waitUntil: (promise: Promise<any>) => {
+						context.waitUntil(promise);
+					},
+					env: env,
+					cf: request.cf,
+					caches: caches as unknown as CacheStorage,
 				},
-				env: env,
-				cf: request.cf,
-				caches: caches as unknown as CacheStorage,
-			},
-		};
+			};
 
-		let response = await app.render(request, routeData, locals);
+			let response = await app.render(request, routeData, locals);
 
-		if (app.setCookieHeaders) {
-			for (const setCookieHeader of app.setCookieHeaders(response)) {
-				response.headers.append('Set-Cookie', setCookieHeader);
+			if (app.setCookieHeaders) {
+				for (const setCookieHeader of app.setCookieHeaders(response)) {
+					response.headers.append('Set-Cookie', setCookieHeader);
+				}
 			}
-		}
 
-		return response;
+			return response;
 	};
 
 	return { default: { fetch } };
