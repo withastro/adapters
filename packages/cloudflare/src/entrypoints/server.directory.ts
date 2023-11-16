376fa39b8bd8ebf038b3a0d95464fb0f69a79693
Alexander Niebuhr <alexander@nbhr.io>
2023-11-16 14:26:17 +0100

    Update server.directory.ts

--- packages/cloudflare/src/entrypoints/server.directory.ts
+++ packages/cloudflare/src/entrypoints/server.directory.ts
@@ -32,7 +32,7 @@ export function createExports(manifest: SSRManifest) {
 			return env.ASSETS.fetch(request);
 		}
 
-		let routeData = app.match(request, { matchNotFound: true });
+		let routeData = app.match(request);
 			Reflect.set(
 				request,
 				Symbol.for('astro.clientAddress'),
