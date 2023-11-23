2ff65a0bcb51f0754939294b412711bbd4a4aded
Simon Knott <info@simonknott.de>
2023-11-23 14:31:05 +0100

    some more change

--- packages/netlify/src/env.d.ts
+++ packages/netlify/src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+import type { Context } from '@netlify/functions';
+
+declare namespace App {
+	interface Locals {
+		netlify: {
+			context: Context;
+		};
+	}
+}
