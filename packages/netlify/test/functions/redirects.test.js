c7522b733580019ed2f32002b4c6d5379a69f55b
Simon Knott <info@simonknott.de>
2024-01-29 16:03:36 +0100

    fix: embed static 404 page

--- packages/netlify/test/functions/redirects.test.js
+++ packages/netlify/test/functions/redirects.test.js
@@ -25,4 +25,19 @@ describe('SSR - Redirects', () => {
 		}
 		expect(hasErrored).to.equal(true, 'this file should not exist');
 	});
+
+	it("renders static 404 page", async () => {
+		const entryURL = new URL(
+			'./fixtures/redirects/.netlify/functions-internal/ssr/ssr.mjs',
+			import.meta.url
+		);
+		const { default: handler } = await import(entryURL);
+		const resp = await handler(
+			new Request('http://example.com/nonexistant-page', ),
+			{}
+		);
+		expect(resp.status).to.equal(404);
+		const text = await resp.text()
+		expect(text).to.contain("This is my static 404 page");
+	})
 });
