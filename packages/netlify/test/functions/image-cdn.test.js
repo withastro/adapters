554cddad1880cc96dfa95ba27b22c40d5e402d8a
Abid Jappie <abidjappie@live.com>
2024-02-08 23:37:15 +0900

    Clean up env in image-cdn.test.

--- packages/netlify/test/functions/image-cdn.test.js
+++ packages/netlify/test/functions/image-cdn.test.js
@@ -1,11 +1,16 @@
 import { loadFixture } from '@astrojs/test-utils';
-import { describe, it, before } from 'node:test';
+import { describe, it, afterEach, beforeEach } from 'node:test';
 import * as assert from 'node:assert/strict';
 
 describe('Image CDN', () => {
 	const root = new URL('./fixtures/middleware/', import.meta.url);
 
 	describe('when running outside of netlify', () => {
+		afterEach(() => {
+			delete process.env.NETLIFY;
+			delete process.env.DISABLE_IMAGE_CDN;
+		});
+
 		it('does not enable Image CDN', async () => {
 			const fixture = await loadFixture({ root });
 			await fixture.build();
@@ -16,6 +21,11 @@ describe('Image CDN', () => {
 	});
 
 	describe('when running inside of netlify', () => {
+		afterEach(() => {
+			delete process.env.NETLIFY;
+			delete process.env.DISABLE_IMAGE_CDN;
+		});
+
 		it('enables Netlify Image CDN', async () => {
 			process.env.NETLIFY = 'true';
 			const fixture = await loadFixture({ root });
@@ -23,8 +33,6 @@ describe('Image CDN', () => {
 
 			const astronautPage = await fixture.readFile('astronaut/index.html');
 			assert.equal(astronautPage.includes(`src="/.netlify/image`),true);
-
-			process.env.NETLIFY = undefined;
 		});
 
 		it('respects image CDN opt-out', async () => {
@@ -35,9 +43,6 @@ describe('Image CDN', () => {
 
 			const astronautPage = await fixture.readFile('astronaut/index.html');
 			assert.equal(astronautPage.includes(`src="/_astro/astronaut.`),true);
-
-			process.env.NETLIFY = undefined;
-			process.env.DISABLE_IMAGE_CDN = undefined;
 		});
 	});
 });
