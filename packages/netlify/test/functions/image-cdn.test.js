afa37eaa7bb45de556d677778e8f3bc2cb611c5d
Abid Jappie <abidjappie@live.com>
2024-02-14 18:57:58 +0900

    All test pass somehow, it may be related to extraneous asynchronous activity.

--- packages/netlify/test/functions/image-cdn.test.js
+++ packages/netlify/test/functions/image-cdn.test.js
@@ -1,16 +1,11 @@
 import { loadFixture } from '@astrojs/test-utils';
-import { describe, it, afterEach, beforeEach } from 'node:test';
+import { describe, it, after } from 'node:test';
 import * as assert from 'node:assert/strict';
 
 describe('Image CDN', () => {
 	const root = new URL('./fixtures/middleware/', import.meta.url);
 
 	describe('when running outside of netlify', () => {
-		afterEach(() => {
-			process.env.NETLIFY = undefined;
-			process.env.DISABLE_IMAGE_CDN = undefined;
-		});
-
 		it('does not enable Image CDN', async () => {
 			const fixture = await loadFixture({ root });
 			await fixture.build();
@@ -18,15 +13,16 @@ describe('Image CDN', () => {
 			const astronautPage = await fixture.readFile('astronaut/index.html');
 			assert.equal(astronautPage.includes(`src="/_astro/astronaut.`),true);
 		});
+
 	});
 
 	describe('when running inside of netlify', () => {
-		afterEach(() => {
+		after(() => {
 			process.env.NETLIFY = undefined;
 			process.env.DISABLE_IMAGE_CDN = undefined;
 		});
 
-		it('enables Netlify Image CDN', async () => {
+		it('enables Netlify Image CDN',async () => {
 			process.env.NETLIFY = 'true';
 			const fixture = await loadFixture({ root });
 			await fixture.build();
