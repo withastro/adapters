f38b3308538fc78853023a1da36e4fb7fbec2889
Alexander Niebuhr <alexander@nbhr.io>
2024-03-25 16:32:02 +0100

    copies new getParts function from Astro

--- packages/cloudflare/src/utils/generate-routes-json.ts
+++ packages/cloudflare/src/utils/generate-routes-json.ts
@@ -11,9 +11,13 @@ import {
 } from '@astrojs/internal-helpers/path';
 import glob from 'tiny-glob';
 
-export const getParts = (part: string) => {
+// Copied from https://github.com/withastro/astro/blob/3776ecf0aa9e08a992d3ae76e90682fd04093721/packages/astro/src/core/routing/manifest/create.ts#L45-L70
+// We're not sure how to improve this regex yet
+const ROUTE_DYNAMIC_SPLIT = /\[(.+?\(.+?\)|.+?)\]/;
+const ROUTE_SPREAD = /^\.{3}.+$/;
+export function getParts(part: string) {
 	const result: RoutePart[] = [];
-	part.split(/\[(.+?\(.+?\)|.+?)\]/).map((str, i) => {
+	part.split(ROUTE_DYNAMIC_SPLIT).map((str, i) => {
 		if (!str) return;
 		const dynamic = i % 2 === 1;
 
@@ -26,12 +30,12 @@ export const getParts = (part: string) => {
 		result.push({
 			content,
 			dynamic,
-			spread: dynamic && /^\.{3}.+$/.test(content),
+			spread: dynamic && ROUTE_SPREAD.test(content),
 		});
 	});
 
 	return result;
-};
+}
 
 const segmentsToCfSyntax = (segments: RouteData['segments'], _config: AstroConfig) => {
 	const pathSegments = [];
