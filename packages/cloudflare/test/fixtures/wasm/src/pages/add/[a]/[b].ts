ff0644f97aae9f357afa04310752d3a08fc3371f
Alexander Niebuhr <alexander@nbhr.io>
2024-04-25 08:55:53 +0200

    add instead of replace tests

--- packages/cloudflare/test/fixtures/wasm/src/pages/add/[a]/[b].ts
+++ packages/cloudflare/test/fixtures/wasm/src/pages/add/[a]/[b].ts
@@ -1,15 +1,17 @@
 import { type APIContext } from 'astro';
-import { add } from '../../../util/add';
+// @ts-ignore
+import mod from '../../../util/add.wasm?module';
 
+const addModule: any = new WebAssembly.Instance(mod);
 
 export const prerender = false;
 
 export async function GET(
   context: APIContext
 ): Promise<Response> {
-	const a = Number.parseInt(context.params.a ?? "0");
-	const b = Number.parseInt(context.params.b ?? "0");
-  return new Response(JSON.stringify({ answer: add(a, b) }), {
+	const a = Number.parseInt(context.params.a!);
+	const b = Number.parseInt(context.params.b!);
+  return new Response(JSON.stringify({ answer: addModule.exports.add(a, b) }), {
     status: 200,
     headers: {
       'Content-Type': 'application/json',
