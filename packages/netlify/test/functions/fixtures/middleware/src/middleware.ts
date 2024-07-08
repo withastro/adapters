325b49446d98b33d440b58ab48e78f1d08285aa1
Alexander Niebuhr <alexander@nbhr.io>
2024-07-08 08:00:26 +0200

    Update packages/netlify/test/functions/fixtures/middleware/src/middleware.ts

--- packages/netlify/test/functions/fixtures/middleware/src/middleware.ts
+++ packages/netlify/test/functions/fixtures/middleware/src/middleware.ts
@@ -3,7 +3,6 @@ import https from 'node:https';
 
 export const onRequest = (context, next) => {
 	context.locals.title = 'Middleware';
-	context.locals.nodeUnprefixedImportExists = !!http;
 	context.locals.nodePrefixedImportExists = !!https;
 
 	return next();
