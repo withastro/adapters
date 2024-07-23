8e377b0a878016de990a9c37d697c44b311b0891
Alexander Niebuhr <alexander@nbhr.io>
2024-07-23 11:06:21 +0200

    fix(cloudflare): add proxy /_image endpoint

--- packages/cloudflare/src/entrypoints/image-endpoint.ts
+++ packages/cloudflare/src/entrypoints/image-endpoint.ts
@@ -1,2 +1,16 @@
-// NOTE: this file is empty on purpose
-// it allows use to offer `imageService: 'compile'`
+import type { APIRoute } from 'astro';
+
+export const prerender = false;
+
+export const GET: APIRoute = (ctx) => {
+	const href = ctx.url.searchParams.get('href');
+	if (!href) {
+		return new Response("Missing 'href' query parameter", { status: 400 });
+	}
+
+	if (!href.startsWith('/_astro')) {
+		return new Response("Invalid 'href' query parameter", { status: 400 });
+	}
+
+	return fetch(new URL(href, ctx.url.origin));
+};
