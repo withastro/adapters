63221a225e824bfe259c3195b3ab095a52c424a3
Alexander Niebuhr <alexander@nbhr.io>
2024-08-03 08:16:37 +0200

    review comments

--- packages/cloudflare/src/entrypoints/image-endpoint.ts
+++ packages/cloudflare/src/entrypoints/image-endpoint.ts
@@ -5,11 +5,10 @@ export const prerender = false;
 export const GET: APIRoute = (ctx) => {
 	const href = ctx.url.searchParams.get('href');
 	if (!href) {
-		return new Response("Missing 'href' query parameter", { status: 400, statusText: "Missing 'href' query parameter" });
-	}
-
-	if (!href.startsWith('/_astro')) {
-		return new Response("Invalid 'href' query parameter", { status: 400 });
+		return new Response("Missing 'href' query parameter", {
+			status: 400,
+			statusText: "Missing 'href' query parameter",
+		});
 	}
 
 	return fetch(new URL(href, ctx.url.origin));
