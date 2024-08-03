809caab2fed4c5030cbafeaaee81776325b82741
Alexander Niebuhr <alexander@nbhr.io>
2024-08-03 08:15:53 +0200

    Update packages/cloudflare/src/entrypoints/image-endpoint.ts
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>

--- packages/cloudflare/src/entrypoints/image-endpoint.ts
+++ packages/cloudflare/src/entrypoints/image-endpoint.ts
@@ -5,7 +5,7 @@ export const prerender = false;
 export const GET: APIRoute = (ctx) => {
 	const href = ctx.url.searchParams.get('href');
 	if (!href) {
-		return new Response("Missing 'href' query parameter", { status: 400 });
+		return new Response("Missing 'href' query parameter", { status: 400, statusText: "Missing 'href' query parameter" });
 	}
 
 	if (!href.startsWith('/_astro')) {
