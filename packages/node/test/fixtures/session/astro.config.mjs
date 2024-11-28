ccfb50ea6bcd9ffa78d0d8c237c492737e69a3a8
Matt Kane <m@mk.gg>
2024-11-28 15:02:35 +0000

    wip

---
+++ packages/node/test/fixtures/session/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+
+export default defineConfig({
+    output: 'server',
+    adapter: node({ mode: 'standalone' })
+});
